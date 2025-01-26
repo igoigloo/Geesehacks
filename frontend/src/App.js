80fb279d7560b47e08f10eebbbd8ae52ffbf915d
Manveer Sohal <77809170+manveer-sohal@users.noreply.github.com>
2025-01-26 03:01:14 -0500

    loadaed and dispalyed camera locations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,75 @@
 import MapComponent from "./components/mapComponent";
 import SidebarComponent from "./components/sidebareComponent";
-
-const accidentData = [
-  { id: 1, lng: -79.3832, lat: 43.6532, description: "Accident at Main St." },
-  { id: 2, lng: -79.3872, lat: 43.6572, description: "Collision near Dundas." },
-];
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [accidentData, setAccidentData] = useState([
+    {
+      id: 100,
+      lng: -79.3832,
+      lat: 43.6532,
+      description: "Accident at Main St.",
+    },
+    {
+      id: 200,
+      lng: -79.3872,
+      lat: 43.6572,
+      description: "Collision near Dundas.",
+    },
+  ]);
+
+  const [cameras, setCameras] = useState([]);
+
+  const [items, setItems] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadData = async () => {
+      // Fetch data from the API
+      const response = await fetch("http://127.0.0.1:8000/cameras", {});
+      const data = await response.json();
+      setItems(data);
+      setLoaded(true);
+    };
+    loadData();
+  }, []); // Empty dependency array means this runs once on mount
+
+  useEffect(() => {
+    if (loaded) {
+      var list = [];
+      items.forEach((camera) => {
+        const newData = {
+          id: camera?.Id,
+          lng: camera?.Longitude,
+          lat: camera?.Latitude,
+          description: camera?.Location,
+        };
+        list.push(newData);
+      });
+
+      setCameras((cameras) => [...cameras, list]);
+      //setAccidentData((accidentData) => [...accidentData, newData]);
+
+      //console.log(accidentData);
+      //setAccidentData((accidentData) => [...accidentData, newData]);
+      //console.log(accidentData);
+    }
+  }, [items]);
+  const click = () => {
+    const newData = {
+      id: items[1]?.Id,
+      lng: -79.3832,
+      lat: 43.6562,
+      description: `Accident at ${items[1]?.Location}.`,
+    };
+    setCameras((cameras) => [...cameras, newData]);
+  };
+
   return (
     <div style={{ display: "flex" }}>
+      <button onClick={click}>click</button>
       <SidebarComponent data={accidentData} />
-      <MapComponent />
+      <MapComponent data={cameras} />
     </div>
   );
 }
--- frontend/src/components/mapComponent.js
+++ frontend/src/components/mapComponent.js
@@ -1,44 +1,68 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
-const Map = () => {
+const Map = ({ data }) => {
   const mapContainerRef = useRef(null);
 
   // Sample accident data
-  const accidentData = [
-    { id: 1, lng: -79.3832, lat: 43.6532, description: "Accident at Main St." },
+  // const accidentData = [
+  //   { id: 1, lng: -79.3832, lat: 43.6532, description: "Accident at Main St." },
+  //   {
+  //     id: 2,
+  //     lng: -79.3872,
+  //     lat: 43.6572,
+  //     description: "Collision near Dundas.",
+  //   },
+  // ];
+  const [accidentData, setAccidentData] = useState([
     {
-      id: 2,
+      id: 100,
+      lng: -79.3832,
+      lat: 43.6532,
+      description: "Accident at Main St.",
+    },
+    {
+      id: 200,
       lng: -79.3872,
       lat: 43.6572,
       description: "Collision near Dundas.",
     },
-  ];
+  ]);
+
+  const [cameras, setCameras] = useState([]);
 
   useEffect(() => {
-    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [-79.3832, 43.6532],
-      zoom: 12,
-    });
+    if (data && data.length > 0) {
+      setCameras(data[0]);
+    }
+  }, [data]);
 
-    // Add markers for accidents
-    accidentData.forEach((accident) => {
-      const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-        accident.description
-      );
+  useEffect(() => {
+    if (cameras.length > 0) {
+      mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+      const map = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [-79.3832, 43.6532],
+        zoom: 12,
+      });
 
-      new mapboxgl.Marker({ color: "red" })
-        .setLngLat([accident.lng, accident.lat])
-        .setPopup(popup)
-        .addTo(map);
-    });
+      // Add markers for accidents
+      cameras.forEach((accident) => {
+        const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+          accident.description
+        );
 
-    return () => map.remove();
-  }, []);
+        new mapboxgl.Marker({ color: "red" })
+          .setLngLat([accident.lng, accident.lat])
+          .setPopup(popup)
+          .addTo(map);
+      });
+
+      return () => map.remove();
+    }
+  }, [cameras]);
 
   return (
     <div ref={mapContainerRef} style={{ width: "100%", height: "100vh" }} />
